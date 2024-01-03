01501957f170501c0b3083760faaefdb54ef75d9
zcemycl <lyc010197@gmail.com>
2024-01-03 00:31:21 +0000

    add navigate project button left-right-top-bot

--- src/project-card.js
+++ src/project-card.js
@@ -1,6 +1,20 @@
 const projectCards = document.querySelector(".project-cards");
-console.log(projectCards);
+const height_const = projectCards.clientHeight;
+let default_height = 0;
+console.log(projectCards, projectCards.clientHeight);
 
-projectCards.addEventListener("mouseover", () => {
-    console.log("hihi")
-})
+// projectCards.addEventListener("mouseover", () => {
+//     console.log("hihi")
+// })
+
+const nav_project_top = () => {
+    default_height += height_const/3*1.3;
+    let projectCards_ = document.querySelector(".project-cards");
+    projectCards_.style.transform = `translateY(${default_height}px) scale(1.3)`
+}
+
+const nav_project_bot = () => {
+    default_height -= height_const/3*1.3;
+    let projectCards_ = document.querySelector(".project-cards");
+    projectCards_.style.transform = `translateY(${default_height}px) scale(1.3)`
+}
