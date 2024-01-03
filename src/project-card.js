32d12b1fad3e1b17c49b8875036da0ecd362f51c
zcemycl <lyc010197@gmail.com>
2024-01-03 01:44:47 +0000

    update

--- src/project-card.js
+++ src/project-card.js
@@ -1,11 +1,34 @@
 const projectCards = document.querySelector(".project-cards");
+const projectRowCards = document.querySelectorAll(".project-row:nth-child(1) .project-card");
+const projectCard = document.querySelector(".project-card");
+const projectRow = document.querySelector(".project-row");
 const height_const = projectCards.clientHeight;
+const width_const = projectCard.clientWidth;
+const cardGap = projectRowCards[1].getBoundingClientRect().left -
+    projectRowCards[0].getBoundingClientRect().left - width_const;
 let default_height = 0;
-console.log(projectCards, projectCards.clientHeight);
+let default_width = 0;
+console.log(projectCards, height_const, width_const, cardGap);
+console.log(projectRowCards[0].offsetLeft)
+console.log(projectRow.getBoundingClientRect(), 
+    projectRowCards[0].getBoundingClientRect(),
+    projectRowCards[1].getBoundingClientRect())
 
 // projectCards.addEventListener("mouseover", () => {
 //     console.log("hihi")
 // })
+
+const nav_project_left = () => {
+    default_width -= (width_const+cardGap);
+    let projectCards_ = document.querySelector(".project-cards");
+    projectCards_.style.transform = `translateX(${default_width}px) scale(1.3)`
+}
+
+const nav_project_right = () => {
+    default_width += (width_const+cardGap);
+    let projectCards_ = document.querySelector(".project-cards");
+    projectCards_.style.transform = `translateX(${default_width}px) scale(1.3)`
+}
 
 const nav_project_top = () => {
     default_height += height_const/3*1.3;
